7c293f93aded735704ab39b75a30434480c94865
Vahnky <vahnky@gmail.com>
2024-02-21 11:25:50 +0100

    style pour la dernière section de cette page d id : play

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-
-
-
-
-var
-
- abChess = {};
-
-var
-
- options = {
-    animated: 
-
-false
-
-
+var abChess = {};
+var options = {
+    animated: false
 };
-abChess = 
 
-new
 
- AbChess(
+function getChessboardWidth() {
+    var screenWidth = window.innerWidth;
+    if (screenWidth <= 400) {
+        return 320;
+    } else if (screenWidth>400 && screenWidth<600) {
+        return 400;
+    }
+    else if (screenWidth>400 && screenWidth<=800){
+        return  600;
+    }
+    else if (screenWidth>800 &&screenwidth<=1000){
+        return 800;
+    }
+    else if(screenWidth>1000 && screenwidth<=1200){
+        return 1000;
+    }
+    else{
+        return 1200;
+    }
+}
 
-"chessboard"
 
-, options);
+options.width = getChessboardWidth();
+
+abChess = new AbChess("chessboard", options);
 abChess.setFEN();
